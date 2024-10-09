85c6095888f3d5ca4ee8496a3399056ca1b89b83
Manuel Markwalder <manuel.markwalder1@swisscom.com>
2024-10-09 20:35:19 +0200

    moving stuff around

---
+++ frontend.react/app/src/app/admin/page.tsx
@@ -0,0 +1,11 @@
+import Container from '../components/container'
+
+const Page = () => (
+  <>
+    <Container>
+      <h1>Admin Section</h1>
+    </Container>
+  </>
+)
+
+export default Page
---
+++ frontend.react/app/src/app/components/header/header.stories.tsx
@@ -0,0 +1,14 @@
+import Header from "."
+
+export default {
+  title: 'molecules/header',
+  component: Header
+}
+
+export const JustAHeader = () => (
+  <>
+    <Header>
+      <h1>Naqab Bedouin Design</h1>
+    </Header>
+  </>
+)
