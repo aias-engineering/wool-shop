e8b456c664fdcbf5c2bdb617ffdc10762315ab7f
Manuel Markwalder <manuel.markwalder1@swisscom.com>
2024-10-31 18:56:05 +0000

    hydration with jotai

--- frontend.react/app/src/app/admin/image/preloaded-image-grid.tsx
+++ frontend.react/app/src/app/admin/image/preloaded-image-grid.tsx
@@ -2,20 +2,19 @@
 
 import ImagesGrid from "@/app/components/organism/images-grid"
 import { imagesFetchAtom } from "@/lib/client/store"
-import { toUrls } from "@/lib/client/store/image"
-import { atom, useSetAtom } from "jotai"
-import { useMemo } from "react"
+import { ImagesFetch, toUrls } from "@/lib/client/store/image"
+import { atom } from "jotai"
+import { useHydrateAtoms } from "jotai/utils"
 
 interface Props {
   imagenames: string[]
 }
 
 export default function PreloadedImagesGrid({imagenames}: Props) {
-  const setImagesFetch = useSetAtom(imagesFetchAtom)
-  useMemo(() => {
-    const data = toUrls(imagenames).map(url => atom({url}))
-    setImagesFetch({step: 'fetched', data })
-  }, [imagenames, setImagesFetch])
+  const data = toUrls(imagenames).map(url => atom({url}))
+  const intialImageFetchState: ImagesFetch = {step: 'fetched', data }
+
+  useHydrateAtoms([[imagesFetchAtom, intialImageFetchState]])
 
   return (
     <ImagesGrid />
