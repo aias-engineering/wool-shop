0a7a57602eb3bd08a02a5123d85a75b97f937ec6
Manuel Markwalder <manuel.markwalder@aias.ch>
2025-01-15 15:43:19 +0100

    some optimation

--- frontend.react/app/src/app/products-shop.tsx
+++ frontend.react/app/src/app/products-shop.tsx
@@ -44,14 +44,15 @@ export default function ProductsShop({ products }: Props) {
           return (
             <div key={product.id} className="flex flex-col gap-2 min-h-[24rem] md:h-[26rem] lg:h-[24rem] xl:h-[36rem]">
               <Link href={`/product/${product.id}`}>
-                <ImageFrame>
+                <ImageFrame className='h-[16rem] md:h-[19rem] lg:h-[17rem] xl:h-[29rem]' >
                   <Image
                     src={product.image}
                     alt={product.name}
-                    sizes="(min-width: 1024px) 25vw, (min-width: 768px) 66vw, 50vw"
+                    sizes="(min-width:1280px) 310px, 172px"
                     width={200}
                     height={300}
                     className="sm:w-full"
+                    
                   />
                 </ImageFrame>
               </Link>
